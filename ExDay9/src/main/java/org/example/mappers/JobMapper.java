537ae74fbf3471bc0ebaa052f0280efda9c9f59d
Sarah Alsunbel <SarahAlsunbel@gmail.com>
2024-06-10 15:13:41 +0300

    Day 9

---
+++ ExDay9/src/main/java/org/example/mappers/JobMapper.java
@@ -0,0 +1,23 @@
+package org.example.mappers;
+
+
+
+import org.example.dto.JobsDto;
+import org.example.models.Jobs;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface JobMapper {
+
+    JobMapper INSTANCE = Mappers.getMapper(JobMapper.class);
+
+
+    //Get
+    JobsDto toDeptDto(Jobs j);
+
+    //Post
+    Jobs toModel(JobsDto dto);
+}
